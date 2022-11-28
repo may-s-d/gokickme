a0dc2f034fcddf3f72788aef663a70a249fc1bdc
Alex Pietrick <61635768+apietrick24@users.noreply.github.com>
2022-11-28 13:49:23 -0500

    Barebones Structure of Account Classes
    -Created files for entities classes
    -Created the User, Admin, Supporter and Designer classes
    -Wrote constructors and get methods for User, Admin, Supporter and Designer classes

---
+++ src/entities/accounts.js
@@ -0,0 +1,73 @@
+//An abstract class that represents a user in GoKickMe
+class User {
+    User(email, name){
+        //If the class that is calling this construct is User
+        if (this.constructor == User) {
+            //Throw an error as User is abstract and cannot be instantiated 
+            throw new Error("Instantiation of an Abstract Class");
+        }
+
+        //Save user's email and name
+        this.email = email
+        this.name = name
+    }
+
+    //get a user's email
+    export getEmail(){
+        return this.email
+    }
+
+    //get a user's name
+    export getName(){
+        return this.name
+    }
+}
+
+//a class (extending User) that represent an dmin in GoKickMe
+class Admin extends User {
+    Admin(email, name){
+        //call the higher class' constructor 
+        super(email, name)
+    }
+}
+
+//a class (extending User) that represent a supporter in GoKickMe
+class Supporter extends User {
+    Supporter(email, name, budget){
+        //call the higher class' constructor 
+        super(email, name)
+
+        //save supporter's budget
+        this.budget = budget
+
+        //Create list for suppoerter's pledges and support
+        this.sucessfulPledges = new []
+        this.directSupports = new []
+    }
+
+    //get a supporter's budget
+    export getBudget(){
+        return this.budget
+    }
+
+    //get a supporter's pledges
+    export getSucessfulPledges(){
+        return this.sucessfulPledges
+    }
+
+    //get a supporter's support
+    export getDirectSupports(){
+        return this.directSupports
+    }
+}
+
+//a class (extending User) that represent a design in GoKickMe
+class Designer extends User {
+    Designer(email, name){
+        //call the higher class' constructor 
+        super(email, name)
+
+        //Create list for designer's projects
+        this.projects = new []
+    }
+}
