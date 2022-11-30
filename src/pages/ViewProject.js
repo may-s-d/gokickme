d1e6be6e5fd4a950edf4adaadf9bc32b31705d01
alex <aemacdonald@wpi.edu>
2022-11-30 14:00:46 -0500

    view project page fleshed out

--- src/pages/ViewProject.js
+++ src/pages/ViewProject.js
@@ -5,10 +5,68 @@ import { aws } from '../AWS.js';
 import Header from '../components/Header.js'
 
 const projectName = window.sessionStorage.getItem("projectName")
+const designerEmail = JSON.parse(window.sessionStorage.getItem('designerEmail'));
+
+const [project, updateProject] = useState();
 
 function ViewProject() {
     console.log(projectName)
-}
+
+    const getProject = () => {
+        const body = {};
+        body['designerEmail'] = designerEmail;
+        body['projectName'] = projectName;
+        const data = { 'body': JSON.stringify(body) }
+        aws.get('/viewProject', data)
+        .then(response => {
+            const project = response.data.body.project;
+            updateProject(project);
+        })
+    }
+
+    const renderProject = () => {
+        return (
+            <Container id={project.projectName} key={index}>
+                { project.projectName }
+                <Button onClick={attemptProjectView}>View</Button>
+                <Button onClick={attemptDeleteProject}>Delete</Button>
+            </Container>
+        )
+    }
+
+    if (typeof projects === 'undefined') {
+        return (
+            <>
+            <Header loggedIn={ true } />
+            <p>Loading projects...</p>
+            { getProject() }
+            </>
+        )
+    }
+
+    else if(!(typeof project === 'undefined')) {
+        return (
+            <>
+                <Navigate 
+                    to={'/viewProject'}
+                />
+            </>
+        )
+    }
+
+    else return (
+        <>
+        <Header loggedIn={ true } />
+        <Container>
+            logged in: { designerEmail }
+            { renderProject() }
+        </Container>
+        </>
+    );
+  }
+
+
+
 
 
 export default ViewProject
