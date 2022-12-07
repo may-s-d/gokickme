a3c07bbf43609f969cedf8c9f37a8fffa3f37874
may-s-d <may.s.dong@gmail.com>
2022-12-06 21:44:13 -0500

    Change PledgeList to use API call /viewPledge

--- src/components/PledgeList.js
+++ src/components/PledgeList.js
@@ -4,77 +4,120 @@ import { aws } from '../AWS.js';
 
 export default function PledgesList(props) { // we assume that either a designer or a supporter is logged in if this is rendering
     const [counter, updateCounter] = useState(0);
+    const [pledges, updatePledges] = useState();
     const project = props.project;
     const designerEmail = window.sessionStorage.getItem('designerEmail');
     const supporterEmail = window.sessionStorage.getItem('supporterEmail');
     const isLaunched = project.launched.data[0] !== 0;
 
-    let rightButton = <></>;
-    if (designerEmail && !isLaunched) {
-        const attemptDelete = (e) => {
-            const body = {};
-            body['designerEmail'] = designerEmail;
-            body['id'] = e.currentTarget.parentNode.parentNode.id;
-            body['projectName'] = project.name;
-            const data = { 'body': JSON.stringify(body) };
-            aws.post('/deletePledge', data)
-            .then(response => {
-                if (response.data.statusCode === 200) {
-                    updateCounter(counter => counter+1);
-                }
-                else {
-                console.log(response.data.body);
-                }
-            })
-            .catch(console.log);
-        }
-        rightButton = <Button variant='danger' onClick={attemptDelete}>Delete</Button>;
-    }
-
-    if (supporterEmail) { // supporter view
-        const attemptClaim = (e) => {
-            const body = {};
-            body['supporterEmail'] = supporterEmail;
-            body['id'] = e.currentTarget.parentNode.parentNode.id;
-            body['projectName'] = project.name;
-            const data = { 'body': JSON.stringify(body) };
-            aws.post('/claimPledge', data)
-            .then(response => {
-                if (response.data.statusCode === 200) {
-                    updateCounter(counter => counter+1);
-                }
-                else {
+    const rightButton = () => {
+        let rightButton = <></>;
+        if (designerEmail && !isLaunched) {
+            const attemptDelete = (e) => {
+                const body = {};
+                body['designerEmail'] = designerEmail;
+                body['id'] = e.currentTarget.parentNode.parentNode.id;
+                body['projectName'] = project.name;
+                const data = { 'body': JSON.stringify(body) };
+                aws.post('/deletePledge', data)
+                .then(response => {
+                    if (response.data.statusCode === 200) {
+                        updateCounter(counter => counter+1);
+                    }
+                    else {
                     console.log(response.data.body);
-                }
-            })
-            .catch(console.log);
+                    }
+                })
+                .catch(console.log);
+            }
+            rightButton = <Button variant='danger' onClick={attemptDelete}>Delete</Button>;
         }
-        rightButton = <Button onClick={attemptClaim}>Claim</Button> /* todo: disable if maxsupporters reached */
-    }
+    
+        else if (supporterEmail && isLaunched) { // supporter view
+            const attemptClaim = (e) => {
+                const body = {};
+                body['supporterEmail'] = supporterEmail;
+                body['id'] = e.currentTarget.parentNode.parentNode.id;
+                body['projectName'] = project.name;
+                const data = { 'body': JSON.stringify(body) };
+                aws.post('/claimPledge', data)
+                .then(response => {
+                    if (response.data.statusCode === 200) {
+                        updateCounter(counter => counter+1);
+                    }
+                    else {
+                        console.log(response.data.body);
+                    }
+                })
+                .catch(console.log);
+            }
+            const disabled = true; // add this in
+            rightButton = <Button onClick={attemptClaim} disabled={disabled}>Claim</Button> /* todo: disable if maxsupporters reached */
+        }
+        
+        return rightButton;
+    } 
+    
 
-    const renderedPledges = project.pledges.map((pledge, index) => {
+    const renderedPledges = pledges.map((pledge, index) => {
         return (
             <tr key={index} id={pledge.id}>
                 <td>${pledge.cost}</td>
                 <td>{pledge.description}</td>
+                <td>here is where i put the # of supporters</td>
                 <td>{pledge.maxSupporters}</td>
-                <td>{ rightButton }</td>
+                <td>{ rightButton() }</td>
             </tr>
         )
     })
-    return (
-        <Table>
-            <thead>
-                <tr>
-                    <th style={{width:'10%'}}>Cost</th>
-                    <th style={{width:'70%'}}>Description</th>
-                    <th style={{width:'10%'}}>Max Supporters</th>
-                    <th style={{width: '10%'}}></th>
-                </tr>
-            </thead>
-            <tbody>
-                { renderedPledges }
-            </tbody>
-        </Table>
-    )
+
+    const getPledges = () => {
+        pledges = [];
+        project.pledges.map((pledge, index) => {
+            body['projectName'] = projectName;
+            body['id'] = pledge.id;
+            const data = { 'body': JSON.stringify(body) }
+            console.log(data);
+            aws.post('/viewPledge', data)
+            .then(response => {
+                if (response.data.statusCode === 200) {
+                    const pledge = response.data.body;
+                    console.log(pledge);
+                    pledges.push(pledge);
+                }
+                else {
+                  console.log(response.data.body);
+                }
+            })
+        })
+        updatePledges(pledges);
+    }
+
+    if (typeof pledges === 'undefined') {
+        return (
+            <>
+            <p>Loading pledge info...</p>
+            { getPledges() }
+            </>
+        )
+    }
+
+    else {
+        return (
+            <Table>
+                <thead>
+                    <tr>
+                        <th style={{width:'10%'}}>Cost</th>
+                        <th style={{width:'60%'}}>Description</th>
+                        <th style={{width:'10%'}}># Supporters</th>
+                        <th style={{width:'10%'}}>Max Supporters</th>
+                        <th style={{width: '10%'}}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { renderedPledges }
+                </tbody>
+            </Table>
+            )
+        }
 }
