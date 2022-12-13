f2659464327ddc374520769606caeee22a2649ed
may-s-d <may.s.dong@gmail.com>
2022-12-13 16:58:19 -0500

    Add more info and button to admin page
    Add delete project buttons on admin homepage and included the status, type, goal amount, and time left on a project

--- src/pages/AdminHomepage.js
+++ src/pages/AdminHomepage.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Table } from 'react-bootstrap';
 import { aws } from '../AWS.js';
 import Header from '../components/Header.js'
 
@@ -20,11 +20,42 @@ function AdminHomepage() {
     }
 
     const renderProjects = () => {
+        const calculateTimeLeft = (deadline) => {
+            let today = Date.parse(new Date())
+            let amongus = Date.parse(deadline)
+            return Math.floor((amongus - today) / 86400000)
+        }
+        const attemptDeleteProject = (e) => {
+            const projectName = e.currentTarget.parentNode.parentNode.id;
+            const body = {};
+            body['adminEmail'] = adminEmail;
+            body['projectName'] = projectName;
+            const data = { 'body': JSON.stringify(body) }
+            console.log(data);
+            aws.post('/deleteProject', data)
+            .then(response => {
+                if (response.data.statusCode === 200) {
+                    updateProjects(undefined); // forces reload
+                }
+                else {
+                    console.log(response.data.body);
+                }
+            })
+            .catch(console.log);
+        }
+        const deleteButton = <Button variant='danger' onClick={attemptDeleteProject}>Delete</Button>;
         const renderedProjects = projects.map((project, index) => {
+            console.log(project);
             return (
-                <Container key={index}>
-                    { project.name }
-                </Container>
+                <tr key={index} id={project.name}>
+                    <td>{ project.name }</td>
+                    <td>{ project.status }</td>
+                    <td>{ project.type }</td>
+                    <td>${ project.goal_amount }</td>
+                    <td>{ calculateTimeLeft(project.deadline) } days</td>
+                    <td>{ deleteButton }</td>
+                    <td></td>
+                </tr>
             )
         });
         return renderedProjects;
@@ -59,7 +90,21 @@ function AdminHomepage() {
             <Header showAccountButtons={ true } loggedIn={ true } />
             <Container>
             <Button variant='danger' onClick={attemptReap}>Reap projects</Button>
-            { renderProjects() }
+                <Table>
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Status</td>
+                            <td>Type</td>
+                            <td>Goal amount</td>
+                            <td>Time left</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { renderProjects() }
+                    </tbody>
+                </Table>
             </Container>
         </>
     )}
